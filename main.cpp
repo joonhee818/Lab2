44ffb8b66f314c5894ce98e4b72e16d6cb3859b1
joonheelee <jhleegraph@snu.ac.kr>
2025-10-07 17:58:21 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
-
-
-
+void func(int start) {
+    if(vec.size() == M){
+        for(int i = 0 ; i < M ; i++)
+            cout << vec[i] << " ";
+        cout << "\n";
+        return;
+    }
+    for(int i = start ; i <= N ; i++){
+        vec.push_back(i);
+        func(i + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(1);
 }
